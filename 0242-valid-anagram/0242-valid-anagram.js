808c562ae79a5eae849522f90ebb242df635d07d
Jake Pearson <97132426+jakepears@users.noreply.github.com>
2023-09-15 21:01:46 -0700

    Time: 56 ms (95.85%), Space: 42.6 MB (85.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+	const map = {};
+	for (let i = 0; i < s.length; i++) {
+		map[s[i]] = (map[s[i]] || 0) + 1;
+	}
+	for (let i = 0; i < t.length; i++) {
+		if (!map[t[i]]) {
+			return false;
+		}
+		map[t[i]]--;
+	}
+	return Object.values(map).every((v) => v === 0);
+};
